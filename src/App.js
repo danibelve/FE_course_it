ff47e88f938557ee821efcb43a5f6f5bf1cada35
danibelve <silvia.belvedere@mercadolibre.com>
2019-08-26 19:53:06 -0300

    [CourseIT] Pruebas de conceptos - clase 4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button/Button'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      FistNumber: '',
+      SecondNumber: '',
+      TotalNumber: ''
+    }
+  }
+
+  handleClick() {
+    const sum = this.state.FistNumber + this.state.SecondNumber;
+    this.setState({
+      TotalNumber: sum
+    })
+  }
+
+  handleChange(e){
+    const name = e.target.name;
+    this.setState({
+      [name]: parseInt(e.target.value)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <main className="App-header">
+
+        <h1>Sumar números</h1>
+
+          <input className="App-input" name="FistNumber" type="text" onChange={e => this.handleChange(e)}></input>
+          <input className="App-input" name="SecondNumber" type="text" onChange={e => this.handleChange(e)}></input>
+
+          <div onClick={() => { this.handleClick() }}>
+            <Button text="Calcular" />
+          </div>
+
+          <h2 className="App-message">{this.state.TotalNumber}</h2>
+
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
